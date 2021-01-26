6b9099f5979b76c15d0a3f99b50f814d8cb0e6ab
Mauricio Maletta <mauricio_maletta@hotmail.com>
2021-01-26 00:00:48 -0300

    add function ping

--- index.js
+++ index.js
@@ -1,5 +1,27 @@
-const Discord = require("discord.js");
+const Discord = require('discord.js');
+
+require('dotenv/config');
+
+console.log('token ', process.env.TOKEN_BOT);
 
 const client = new Discord.Client();
 
-client.login(process.env.BOT_TOKEN);
+const prefix = '@';
+
+client.on('message', (message) => {
+  // ignore bots messages
+  if (message.author.bot) return;
+  // ignore messages that dont start with prefix
+  if (!message.content.startsWith(prefix)) return;
+
+  const commandBody = message.content.slice(prefix.length);
+  const args = commandBody.split(' ');
+  const command = args.shift().toLowerCase();
+
+  if (command === 'ping') {
+    const timeTaken = Date.now() - message.createdTimestamp;
+    message.reply(`Pong! Essa mensagem teve a latência de ${timeTaken}ms.`);
+  }
+});
+
+client.login(process.env.TOKEN_BOT);
