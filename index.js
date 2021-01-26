8978ad2c5419c0a93f092eb69b3a5f3776dbe2d7
Mauricio Maletta <mauricio_maletta@hotmail.com>
2021-01-26 00:32:42 -0300

    change default port

--- index.js
+++ index.js
@@ -33,6 +33,6 @@ const express = require('express');
 
 const app = express();
 app.get('/', (req, res) => res.json({ token: process.env.TOKEN_BOT }));
-app.listen(process.env.PORT || 3000, () => {
-  console.log('Server running on port 3000');
+app.listen(process.env.PORT || 80, () => {
+  console.log('Server running on port 80');
 });
